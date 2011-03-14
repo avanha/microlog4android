62aada8984342f8defa8d6416f738db04902a5a7
Andre Vanha <avanha@photobucket.com>
2011-03-14 00:19:04 -0600

    Removed @Override annotations on methods that only implement an interface but don't actually override anything to get rid of Eclipse compilation errors.

--- microlog4android-integration-tests/src/test/java/com/google/code/microlog4android/integration/tests/appender/JunitTestAppender.java
+++ microlog4android-integration-tests/src/test/java/com/google/code/microlog4android/integration/tests/appender/JunitTestAppender.java
@@ -11,43 +11,42 @@ import com.google.code.microlog4android.format.Formatter;
 public class JunitTestAppender implements Appender {
 	private List<String> loggerList = new ArrayList<String>();
 
-	@Override
 	public void clear() {
 	}
 
-	@Override
 	public void close() throws IOException {
 	}
 
-	@Override
 	public void doLog(String clientID, String name, long time, Level level, Object message, Throwable t) {
 		loggerList.add((String)message);
 	}
 
-	@Override
 	public Formatter getFormatter() {
 		return null;
 	}
 
-	@Override
 	public long getLogSize() {
 		return 0;
 	}
 
-	@Override
 	public boolean isLogOpen() {
 		return false;
 	}
 
-	@Override
 	public void open() throws IOException {
 	}
 
-	@Override
 	public void setFormatter(Formatter arg0) {
 	}
 	
 	public List<String> getLoggerList() {
 		return loggerList;
+	}
+	
+	public String[] getPropertyNames() {
+		return null;
+	}
+	
+	public void setProperty(String name, String value) {
 	}
 }
